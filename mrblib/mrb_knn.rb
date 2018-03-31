bf602f7de110206e1ca1371abbc80aabffee6a34
tsurubee <29603512+tsurubee@users.noreply.github.com>
2018-04-01 00:28:58 +0900

    Add dist function to calculate distace between vectors

--- mrblib/mrb_knn.rb
+++ mrblib/mrb_knn.rb
@@ -1,5 +1,11 @@
 class KNN
-  def bye
-    "bye"
+
+  def dist(vec1, vec2)
+    sum = 0
+    vec1.each_with_index do |v, i|
+      sum += (v - vec2[i])**2
+    end
+    return Math.sqrt(sum)
   end
+
 end
