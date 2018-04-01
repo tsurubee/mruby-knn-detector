f63b609d779ffde85f1af9a8917c8ef40cfd4cb6
tsurubee <29603512+tsurubee@users.noreply.github.com>
2018-04-01 11:17:20 +0900

    Add function to calculate anomaly scores

--- mrblib/mrb_knn.rb
+++ mrblib/mrb_knn.rb
@@ -1,13 +1,28 @@
 class KNN
 
+  def score(data, w, k)
+    windows = sliding_windows(data, w)
+    w_size  = windows.length
+    scores  = []
+
+    w_size.times do |t|
+      distances = []
+      windows.each do |win|
+        distances << dist(windows[t], win)
+      end
+      distances.sort!
+      s = distances[-k..-1].inject(0.0){|r,i| r+=i} / k
+      scores << s
+    end
+    return scores
+  end
+
   def sliding_windows(data, w)
     num     = data.length - w + 1
-    start = 0
     windows = []
 
-    num.times do
-      windows << data[start, w]
-      start += 1
+    num.times do |t|
+      windows << data[t, w]
     end
     return windows
   end
